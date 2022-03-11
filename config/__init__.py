843607ffaea2167a634ed44314f993c1113d8225
KEN KAN <84455320+kenkansaja@users.noreply.github.com>
2022-03-11 14:10:17 +0700

    Add files via upload

---
+++ config/__init__.py
@@ -0,0 +1,11 @@
+#
+# Copyright (C) 2021-2022 by kenkansaja@Github, < https://github.com/kenkansaja >.
+#
+# This file is part of < https://github.com/kenkansaja/Musikku > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/kenkansaja/Musikku/blob/master/LICENSE >
+#
+# All rights reserved.
+#
+
+from .config import *
