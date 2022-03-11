53c92bce042bed07da0b4a89879ccba7ea5db3b1
KEN KAN <84455320+kenkansaja@users.noreply.github.com>
2022-03-11 14:12:38 +0700

    rename

---
+++ Musikku/__init__.py
@@ -0,0 +1,47 @@
+#
+# Copyright (C) 2021-2022 by kenkansaja@Github, < https://github.com/kenkansaja >.
+#
+# This file is part of < https://github.com/kenkansaja/Musikku > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/kenkansaja/Musikku/blob/master/LICENSE >
+#
+# All rights reserved.
+
+from Musikku.core.bot import MusikkuBot
+from Musikku.core.dir import dirr
+from YukkiMusic.core.git import git
+from Musikku.core.userbot import Userbot
+from Musikku.misc import dbb, heroku, sudo
+
+from .logging import LOGGER
+
+# Directories
+dirr()
+
+#Check Git Updates
+git()
+
+# Initialize Memory DB
+dbb()
+
+# Heroku APP
+heroku()
+
+# Load Sudo Users from DB
+sudo()
+
+# Bot Client
+app = MusikkuBot()
+
+# Assistant Client
+userbot = Userbot()
+
+from .platforms import *
+
+YouTube = YouTubeAPI()
+Carbon = CarbonAPI()
+Spotify = SpotifyAPI()
+Apple = AppleAPI()
+Resso = RessoAPI()
+SoundCloud = SoundAPI()
+Telegram = TeleAPI()
---
+++ Musikku/__main__.py
@@ -0,0 +1,81 @@
+#
+# Copyright (C) 2021-2022 by kenkansaja@Github, < https://github.com/kenkansaja >.
+#
+# This file is part of < https://github.com/kenkansaja/Musikku > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/kenkansaja/Musikku/blob/master/LICENSE >
+#
+# All rights reserved.
+
+import asyncio
+import importlib
+import sys
+
+from pyrogram import idle
+from pytgcalls.exceptions import NoActiveGroupCall
+
+import config
+from config import BANNED_USERS
+from Musikku import LOGGER, app, userbot
+from Musikku.core.call import Musikku
+from Musikku.plugins import ALL_MODULES
+from Musikku.utils.database import get_banned_users, get_gbanned
+
+loop = asyncio.get_event_loop()
+
+
+async def init():
+    if (
+        not config.STRING1
+        and not config.STRING2
+        and not config.STRING3
+        and not config.STRING4
+        and not config.STRING5
+    ):
+        LOGGER("Musikku").error(
+            "No Assistant Clients Vars Defined!.. Exiting Process."
+        )
+        return
+    if (
+        not config.SPOTIFY_CLIENT_ID
+        and not config.SPOTIFY_CLIENT_SECRET
+    ):
+        LOGGER("MusikkuMusic").warning(
+            "No Spotify Vars defined. Your bot won't be able to play spotify queries."
+        )
+    try:
+        users = await get_gbanned()
+        for user_id in users:
+            BANNED_USERS.add(user_id)
+        users = await get_banned_users()
+        for user_id in users:
+            BANNED_USERS.add(user_id)
+    except:
+        pass
+    await app.start()
+    for all_module in ALL_MODULES:
+        importlib.import_module("Musikku.plugins" + all_module)
+    LOGGER("Musikku.plugins").info(
+        "Successfully Imported Modules "
+    )
+    await userbot.start()
+    await Musikku.start()
+    try:
+        await Musikku.stream_call(
+            "http://docs.evostream.com/sample_content/assets/sintel1m720p.mp4"
+        )
+    except NoActiveGroupCall:
+        LOGGER("Musikku").error(
+            "[ERROR] - \n\nPlease turn on your Logger Group's Voice Call. Make sure you never close/end voice call in your log group"
+        )
+        sys.exit()
+    except:
+        pass
+    await Musikku.decorators()
+    LOGGER("Musikku").info("Musikku Music Bot Started Successfully")
+    await idle()
+
+
+if __name__ == "__main__":
+    loop.run_until_complete(init())
+    LOGGER("Musikku").info("Stopping Musikku Music Bot! GoodBye")
