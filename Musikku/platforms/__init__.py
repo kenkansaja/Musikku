f998170c7f808ab040c64b99d75ef70192907ae0
KEN KAN <84455320+kenkansaja@users.noreply.github.com>
2022-03-11 14:25:35 +0700

    kanger

--- Musikku/platforms/__init__.py
+++ Musikku/platforms/__init__.py
@@ -1,1 +1,16 @@
+#
+# Copyright (C) 2021-2022 by kenkansaja@Github, < https://github.com/kenkansaja >.
+#
+# This file is part of < https://github.com/kenkansaja/Musikku > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/kenkansaja/Musikku/blob/master/LICENSE >
+#
+# All rights reserved.
 
+from .Apple import AppleAPI
+from .Carbon import CarbonAPI
+from .Resso import RessoAPI
+from .Soundcloud import SoundAPI
+from .Spotify import SpotifyAPI
+from .Telegram import TeleAPI
+from .Youtube import YouTubeAPI
