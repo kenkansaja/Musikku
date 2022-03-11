a9576d608987b2cc5caa45ca9c018e722998a758
KEN KAN <84455320+kenkansaja@users.noreply.github.com>
2022-03-11 14:22:19 +0700

    ppk

---
+++ Musikku/utils/decorators/language.py
@@ -0,0 +1,40 @@
+#
+# Copyright (C) 2021-2022 by kenkansaja@Github, < https://github.com/kenkansaja >.
+#
+# This file is part of < https://github.com/kenkansaja/Musikku > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/kenkansaja/Musikku/blob/master/LICENSE >
+#
+# All rights reserved.
+
+from strings import get_string
+from Musikku.utils.database import get_lang, is_commanddelete_on
+
+
+def language(mystic):
+    async def wrapper(_, message, **kwargs):
+        if await is_commanddelete_on(message.chat.id):
+            try:
+                await message.delete()
+            except:
+                pass
+        try:
+            language = await get_lang(message.chat.id)
+            language = get_string(language)
+        except:
+            language = get_string("id")
+        return await mystic(_, message, language)
+
+    return wrapper
+
+
+def languageCB(mystic):
+    async def wrapper(_, CallbackQuery, **kwargs):
+        try:
+            language = await get_lang(CallbackQuery.message.chat.id)
+            language = get_string(language)
+        except:
+            language = get_string("en")
+        return await mystic(_, CallbackQuery, language)
+
+    return wrapper
