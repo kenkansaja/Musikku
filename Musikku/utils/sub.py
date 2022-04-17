efb0427b4cd7a42b1405d6f2b579921dcf223613
ajual7832@gmail.com <ajual7832@gmail.com>
2022-04-18 04:57:46 +0700

    gak jadi lang

--- Musikku/utils/sub.py
+++ Musikku/utils/sub.py
@@ -3,16 +3,17 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 
 from Musikku import app
 import config
-from strings import get_string
+
 
 def ken(func):
     async def wrapper(_, message: Message):
+        user_id = message.from_user.id
+        user_name = message.from_user.first_name
+        rpk = "[" + user_name + "](tg://user?id=" + str(user_id) + ")"
         if not config.MUST_JOIN:  # Not compulsory
             return
         try:
             try:
-                language = await get_lang(message.chat.id)
-                       _ = get_string(language)
                 await app.get_chat_member(config.MUST_JOIN, message.from_user.id)
             except UserNotParticipant:
                 if config.MUST_JOIN.isalpha():
@@ -21,17 +22,18 @@ def ken(func):
                     chat_info = await app.get_chat(config.MUST_JOIN)
                     chat_info.invite_link
                 try:
-                    await app.reply_text(_["subcribe"],
+                    await message.reply(
+                        f"**Hay kak {rpk}, Silahkan join dulu biar bisa pake bot ini kak**",
                         disable_web_page_preview=True,
                         reply_markup=InlineKeyboardMarkup(
-                            [[InlineKeyboardButton("••ᴊᴏɪɴ••", url=link)]]
+                            [[InlineKeyboardButton("••ꜱɪʟᴀʜᴋᴀɴ ᴊᴏɪɴ••", url=link)]]
                         ),
                     )
                     await message.stop_propagation()
                 except ChatWriteForbidden:
                     pass
         except ChatAdminRequired:
-            await app.reply_text(
+            await message.reply(
                 f"Saya bukan admin di chat MUST_JOIN chat : {config.MUST_JOIN} !"
             )
         return await func(_, message)
