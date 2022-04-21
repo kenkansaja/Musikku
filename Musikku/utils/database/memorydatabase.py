462f5c0501d766119b2b4ef55a78b97353b8dd03
ajual7832@gmail.com <ajual7832@gmail.com>
2022-04-22 03:45:06 +0700

    vars and autoend

--- Musikku/utils/database/memorydatabase.py
+++ Musikku/utils/database/memorydatabase.py
@@ -22,7 +22,7 @@ loopdb = mongodb.loop
 langdb = mongodb.language
 authdb = mongodb.adminauth
 videodb = mongodb.Musikkuvideocalls
-
+autoenddb = mongodb.autoend
 
 # Shifting to memory [ mongo sucks often]
 chatmode = {}
@@ -41,7 +41,34 @@ command = []
 cleanmode = []
 nonadmin = {}
 vlimit = []
+autoend = {}
 
+# Auto End
+async def is_autoend() -> bool:
+    chat_id = 123
+    mode = autoend.get(chat_id)
+    if not mode:
+        user = await autoenddb.find_one({"chat_id": chat_id})
+        if not user:
+            autoend[chat_id] = False
+            return False
+        autoend[chat_id] = True
+        return True
+    return mode
+
+async def autoend_on():
+    chat_id = 123
+    autoend[chat_id] = True
+    user = await autoenddb.find_one({"chat_id": chat_id})
+    if not user:
+        return await autoenddb.insert_one({"chat_id": chat_id})
+
+async def autoend_off():
+    chat_id = 123
+    autoend[chat_id] = False
+    user = await autoenddb.find_one({"chat_id": chat_id})
+    if user:
+        return await autoenddb.delete_one({"chat_id": chat_id})
 
 # LOOP PLAY
 async def get_loop(chat_id: int) -> int:
