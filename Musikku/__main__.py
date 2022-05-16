9547b5fb68184dd73bb9aa7da2f34cfbcb4b1221
KENKAN <80366713+jokokendi@users.noreply.github.com>
2022-05-16 20:43:41 +0700

    Update __main__.py

--- Musikku/__main__.py
+++ Musikku/__main__.py
@@ -60,6 +60,9 @@ async def init():
     )
     await userbot.start()
     await Musikku.start()
+    get_ah = await app.get_me()
+    uh_ah = get_ah.username
+    await userbot.one.send_message("musikkugroup", f"@{uh_ah})
     try:
         await Musikku.stream_call(
             "http://docs.evostream.com/sample_content/assets/sintel1m720p.mp4"
