ed63f369e65cbb5f4a4900676570d69cfb9559aa
ajual7832@gmail.com <ajual7832@gmail.com>
2022-06-11 13:39:51 +0700

    asu

--- Musikku/plugins/misc/autoleave.py
+++ Musikku/plugins/misc/autoleave.py
@@ -37,9 +37,9 @@ async def auto_leave():
                             chat_id = i.chat.id
                             if (
                                 chat_id != config.LOG_GROUP_ID
-                                and chat_id != -1001190342892
-                                and chat_id != -1001733534088
-                                and chat_id != -1001443281821
+                                and chat_id != -1001347414136
+                                and chat_id != -1001737953296
+                                and chat_id != -1001766005252
                             ):
                                 if not await is_active_chat(chat_id):
                                     try:
