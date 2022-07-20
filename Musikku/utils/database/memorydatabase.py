f7e1c7413616c7d33d3b649ce3a98fb445412df7
KENKAN <80366713+jokokendi@users.noreply.github.com>
2022-07-20 19:38:18 +0700

    id

--- Musikku/utils/database/memorydatabase.py
+++ Musikku/utils/database/memorydatabase.py
@@ -184,8 +184,8 @@ async def get_lang(chat_id: int) -> str:
     if not mode:
         lang = await langdb.find_one({"chat_id": chat_id})
         if not lang:
-            langm[chat_id] = "en"
-            return "en"
+            langm[chat_id] = "id"
+            return "id"
         langm[chat_id] = lang["lang"]
         return lang["lang"]
     return mode
