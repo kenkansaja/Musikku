86add7b282beee3ea3f906375b56c33fc847297c
KENKAN <80366713+jokokendi@users.noreply.github.com>
2022-07-28 06:17:53 +0700

    hems

--- Musikku/core/userbot.py
+++ Musikku/core/userbot.py
@@ -57,9 +57,8 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat(-1001347414136)
-                await self.one.join_chat(-1001737953296)
-                await self.one.join_chat("Jasadeploy")
+                await self.one.join_chat("aruna_mutual")
+                await self.one.join_chat("gabutan_escape")
             except:
                 pass
             assistants.append(1)
@@ -88,9 +87,8 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat(-1001347414136)
-                await self.two.join_chat(-1001737953296)
-                await self.two.join_chat("Jasadeploy")
+                await self.one.join_chat("aruna_mutual")
+                await self.one.join_chat("gabutan_escape")      
             except:
                 pass
             assistants.append(2)
@@ -119,9 +117,8 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat(-1001347414136)
-                await self.three.join_chat(-1001737953296)
-                await self.three.join_chat("Jasadeploy")
+                await self.one.join_chat("aruna_mutual")
+                await self.one.join_chat("gabutan_escape")               
             except:
                 pass
             assistants.append(3)
@@ -150,9 +147,8 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat(-1001347414136)
-                await self.four.join_chat(-1001737953296)
-                await self.four.join_chat("Jasadeploy")
+                await self.one.join_chat("aruna_mutual")
+                await self.one.join_chat("gabutan_escape")             
             except:
                 pass
             assistants.append(4)
@@ -181,9 +177,8 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat(-1001347414136)
-                await self.five.join_chat(-1001737953296)
-                await self.five.join_chat("Jasadeploy")
+                await self.one.join_chat("aruna_mutual")
+                await self.one.join_chat("gabutan_escape")             
             except:
                 pass
             assistants.append(5)
