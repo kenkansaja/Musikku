1964f40cdd10aa159ea87d7ba7635f71c8c35426
Azkiya <88168463+EmiliaTzy@users.noreply.github.com>
2022-09-02 13:44:54 +0200

    Update language.py

--- Musikku/utils/decorators/language.py
+++ Musikku/utils/decorators/language.py
@@ -19,7 +19,7 @@ def language(mystic):
         if await is_maintenance() is False:
             if message.from_user.id not in SUDOERS:
                 return await message.reply_text(
-                    "Bot is under maintenance. Please wait for some time..."
+                    "Bot sedang maintenance. Mohon Tunggu Beberapa Menit..."
                 )
         if await is_commanddelete_on(message.chat.id):
             try:
@@ -41,7 +41,7 @@ def languageCB(mystic):
         if await is_maintenance() is False:
             if CallbackQuery.from_user.id not in SUDOERS:
                 return await CallbackQuery.answer(
-                    "Bot is under maintenance. Please wait for some time...",
+                    "Bot sedang maintenance. Mohon Tunggu Beberapa Menit...",
                     show_alert=True,
                 )
         try:
