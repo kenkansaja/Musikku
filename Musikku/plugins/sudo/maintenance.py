723333fc9f2f35bd2f675703135b868f0c117bfd
Azkiya <88168463+EmiliaTzy@users.noreply.github.com>
2022-09-02 13:31:23 +0200

    Update maintenance.py

--- Musikku/plugins/sudo/maintenance.py
+++ Musikku/plugins/sudo/maintenance.py
@@ -38,7 +38,7 @@ async def maintenance(client, message: Message):
     if state == "enable":
         if await is_maintenance() is False:
             await message.reply_text(
-                "Maintenance mode is already enabled"
+                "Mode Maintenance Telah Aktif"
             )
         else:
             await maintenance_on()
@@ -49,7 +49,7 @@ async def maintenance(client, message: Message):
             await message.reply_text(_["maint_3"])
         else:
             await message.reply_text(
-                "Maintenance mode is already disabled"
+                "Mode Maintenance Telah Nonaktif"
             )
     else:
         await message.reply_text(usage)
