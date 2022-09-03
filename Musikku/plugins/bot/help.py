00eed05e01aeebcb9acf775eb6b4aa629420037f
Azkiya <88168463+EmiliaTzy@users.noreply.github.com>
2022-09-03 02:28:23 +0200

    Update help.py

--- Musikku/plugins/bot/help.py
+++ Musikku/plugins/bot/help.py
@@ -95,7 +95,7 @@ async def helper_cb(client, CallbackQuery, _):
     if cb == "hb5":
         if CallbackQuery.from_user.id not in SUDOERS:
             return await CallbackQuery.answer(
-                "Only for Sudo Users", show_alert=True
+                "Hanya Untuk Pengguna Sudo", show_alert=True
             )
         else:
             await CallbackQuery.edit_message_text(
