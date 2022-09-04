65099e6c81b7eec3ed48d01ffaffe8d4e1ee99eb
Azkiya <88168463+EmiliaTzy@users.noreply.github.com>
2022-09-04 08:15:07 +0200

    Update inline.py

--- Musikku/plugins/bot/inline.py
+++ Musikku/plugins/bot/inline.py
@@ -47,24 +47,24 @@ async def inline_query_handler(client, query):
                 [
                     [
                         InlineKeyboardButton(
-                            text="🎥 Watch on Youtube",
+                            text="🎥 Nonton Di Youtube",
                             url=link,
                         )
                     ],
                 ]
             )
             searched_text = f"""
-❇️**Title:** [{title}]({link})
+❇️**Judul:** [{title}]({link})
 
-⏳**Duration:** {duration} Mins
-👀**Views:** `{views}`
-⏰**Published Time:** {published}
-🎥**Channel Name:** {channel}
-📎**Channel Link:** [Visit From Here]({channellink})
+⏳**Durasi:** {duration} Menit
+👀**Penonton:** `{views}`
+⏰**Waktu Publikasi:** {published}
+🎥**Nama Channel:** {channel}
+📎**Tautan Channel:** [Kunjungi Dari Sini]({channellink})
 
-__Reply with /play on this searched message to stream it on voice chat.__
+__Balas dengan /play pada pesan yang dicari ini untuk mengalirkannya di obrolan suara.__
 
-⚡️ ** Inline Search By {MUSIC_BOT_NAME} **"""
+⚡️ ** Pencarian Inline Oleh {MUSIC_BOT_NAME} **"""
             answers.append(
                 InlineQueryResultPhoto(
                     photo_url=thumbnail,
