24fc5e7302e88e4a79775020dd56f1a588adf82a
Azkiya <88168463+EmiliaTzy@users.noreply.github.com>
2022-09-05 13:05:05 +0200

    Update autoend.py

--- Musikku/plugins/sudo/autoend.py
+++ Musikku/plugins/sudo/autoend.py
@@ -22,7 +22,7 @@ AUTOEND_COMMAND = get_command("AUTOEND_COMMAND")
 
 @app.on_message(filters.command(AUTOEND_COMMAND) & SUDOERS)
 async def auto_end_stream(client, message):
-    usage = "**Usage:**\n\n/autoend [enable|disable]"
+    usage = "**Penggunaan:**\n\n/autoend [enable|disable]"
     if len(message.command) != 2:
         return await message.reply_text(usage)
     state = message.text.split(None, 1)[1].strip()
@@ -30,10 +30,10 @@ async def auto_end_stream(client, message):
     if state == "enable":
         await autoend_on()
         await message.reply_text(
-            "Auto End Stream Enabled.\n\nBot will leave voice chat automatically after 3 mins if no one is listening with a warning message.."
+            "Auto End Stream Diaktifkan.\n\nBot akan meninggalkan obrolan suara secara otomatis setelah 3 menit jika tidak ada yang mendengarkan dengan pesan peringatan.."
         )
     elif state == "disable":
         await autoend_off()
-        await message.reply_text("Auto End Stream Disabled.")
+        await message.reply_text("Auto End Stream Dinonaktifkan.")
     else:
         await message.reply_text(usage)
