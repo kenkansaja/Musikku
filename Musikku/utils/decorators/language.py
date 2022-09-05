fd053187a94148258c35549724649a038d063d70
Azkiya <88168463+EmiliaTzy@users.noreply.github.com>
2022-09-05 13:17:52 +0200

    Update language.py

--- Musikku/utils/decorators/language.py
+++ Musikku/utils/decorators/language.py
@@ -30,7 +30,7 @@ def language(mystic):
             language = await get_lang(message.chat.id)
             language = get_string(language)
         except:
-            language = get_string("en")
+            language = get_string("id")
         return await mystic(_, message, language)
 
     return wrapper
@@ -48,7 +48,7 @@ def languageCB(mystic):
             language = await get_lang(CallbackQuery.message.chat.id)
             language = get_string(language)
         except:
-            language = get_string("en")
+            language = get_string("id")
         return await mystic(_, CallbackQuery, language)
 
     return wrapper
@@ -60,7 +60,7 @@ def LanguageStart(mystic):
             language = await get_lang(message.chat.id)
             language = get_string(language)
         except:
-            language = get_string("en")
+            language = get_string("id")
         return await mystic(_, message, language)
 
     return wrapper
